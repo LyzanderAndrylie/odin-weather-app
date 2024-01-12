f311bf41fc87cdbe961ea9bbefe22601eb75ee04
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-01-12 18:41:47 +0700

    Fix protocol

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './main.css';
 
 const API_KEY = '64639162cde7412b81275428241201';
-const BASE_URL = 'http://api.weatherapi.com/v1';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 
 type Weather = {
   current: {
